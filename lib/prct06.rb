05d09da079d17f43aae2e211a750787fb063f2d7
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-10-24 11:25:41 +0100

    Cambiamos el nombre de las variables para seguir un mismo patrón (Todos los nombres en Español)

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -2,11 +2,11 @@ require "prct06/version"
 
 module Prct06
 	class Alimento
-		attr_reader :name, :proteins
+		attr_reader :nombre, :proteinas
 		
 		def initialize(nombre, proteinas)
-			@name = nombre
-			@proteins = proteinas
+			@nombre = nombre
+			@proteinas = proteinas
 		end
 	end
 end
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -13,11 +13,12 @@ RSpec.describe Prct06 do
 		@alimento = Prct06::Alimento.new("Nombre", 10)
 	end
 	
-	it "has 'name' attribute" do
-		expect(@alimento.name).not_to be nil
+	it "has 'nombre' attribute" do
+		expect(@alimento.nombre).not_to be nil
 	end
 	
-	it "has 'proteins' attribute" do
-		expect(@alimento.proteins).not_to be nil
+	it "has 'proteinas' attribute" do
+		expect(@alimento.proteinas).not_to be nil
 	end
+	
 end
