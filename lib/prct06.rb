00ad08654305fe5768069c68ddb55faff482c776
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-10-24 11:23:15 +0100

    Prueba 4, pasada

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -2,10 +2,11 @@ require "prct06/version"
 
 module Prct06
 	class Alimento
-		attr_reader :name
+		attr_reader :name, :proteins
 		
-		def initialize(nombre)
+		def initialize(nombre, proteinas)
 			@name = nombre
+			@proteins = proteinas
 		end
 	end
 end
