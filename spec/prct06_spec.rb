1d971619925eed7179d124c00ff9ce129d875cd8
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-10-24 10:48:58 +0100

    Primer test pasado

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -4,8 +4,5 @@ RSpec.describe Prct06 do
   it "has a version number" do
     expect(Prct06::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+	
 end
