0cacc9f95fd03d552da77abc41c64137f09231ac
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-10-24 11:02:29 +0100

    Prueba 2

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -1,8 +1,15 @@
 require "spec_helper"
+require "prct06"
 
 RSpec.describe Prct06 do
-  it "has a version number" do
-    expect(Prct06::VERSION).not_to be nil
-  end
+	it "has a version number" do
+		expect(Prct06::VERSION).not_to be nil
+	end
 	
+end
+
+RSpec.describe Prct06 do
+	before :each do
+		@alimento = Alimento
+	end
 end
