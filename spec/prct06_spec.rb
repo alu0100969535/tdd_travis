c269f86e48fcc4370b0770af2f5565212216c968
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-10-25 10:25:44 +0100

    Pruebas adicionales para 6 alimentos

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -49,4 +49,31 @@ RSpec.describe "User made tests" do
 			expect(@alimento.to_s).to eq(@to_s)
 		end
 	end
+	
+	context "Pruebas a 6 alimentos diferentes" do
+		before :each do
+			@Proteinas = [1.2, 0.3, 1.3, 1.0, 2.0, 23.5]
+			@Glucidos = [21.4, 12.8, 5.8, 3.5, 15.4, 52.0]
+			@Lipidos = [0.2, 0.4, 0.3, 0.2, 0.1, 1.4]
+			
+			@alimento1 = Prct06::Alimento.new("Plátanos", @Proteinas[0], @Glucidos[0], @Lipidos[0])
+			@alimento2 = Prct06::Alimento.new("Manzana", @Proteinas[1], @Glucidos[1], @Lipidos[1])
+			@alimento3 = Prct06::Alimento.new("Cebolla", @Proteinas[2], @Glucidos[2], @Lipidos[2])
+			@alimento4 = Prct06::Alimento.new("Tomate", @Proteinas[3], @Glucidos[3], @Lipidos[3])
+			@alimento5 = Prct06::Alimento.new("Papas", @Proteinas[4], @Glucidos[4], @Lipidos[4])
+			@alimento6 = Prct06::Alimento.new("Lentejas", @Proteinas[5], @Glucidos[5], @Lipidos[5])
+		end
+			
+		context "Cálculo" do		
+			[0, 1, 2, 3, 4, 5].each do |x|
+				it "Alimento#{x+1}" do
+					alimentos = [@alimento1, @alimento2, @alimento3, @alimento4, @alimento5, @alimento6]
+					expect(alimentos[x].calcular).to eq(@Proteinas[x]*4 + @Glucidos[x]*4 + @Lipidos[x]*9)
+				end
+			end
+		end
+	end
+	
 end
+
+
