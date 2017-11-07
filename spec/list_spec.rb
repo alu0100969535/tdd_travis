05f738e765be86746ed3c885f2ce8ce89882792d
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-11-07 20:35:12 +0000

    Prueba 3 list fallo

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe "List tests" do
 		it "Instanciando un objeto lista" do
 			expect(@lista.class).to be Lista
 		end
+		
+		it "Tiene el método 'head'" do
+			expect(@lista.head).not_to be nil
+		end
 	end
 end
