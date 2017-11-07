d6b2ea6d4a42a901c623ec8e344a2723e42a6f67
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-11-07 16:44:50 +0000

    Prueba 2 list fallo

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,1 +1,12 @@
 require "list"
+
+RSpec.describe "List tests" do
+	context "Probando los métodos" do
+		before :each do
+			@lista = Lista.new();
+		end
+		it "Instanciando un objeto lista" do
+			expect(@lista.class).to be Lista
+		end
+	end
+end
