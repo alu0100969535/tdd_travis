8ff382e4ff2fe1809edc08069cdb5b647e36e816
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-11-07 21:10:33 +0000

    Prueba 7 list pasada

--- lib/list.rb
+++ lib/list.rb
@@ -1,13 +1,28 @@
 Node = Struct.new(:value, :next, :prev)
 
 class Lista
-	attr_reader :head, :tail
+	attr_reader :head,:tail
 	
 	def initialize 
 		@head = Node.new(0,nil,nil)
 		@tail = @head
 	end
 	
+	def insert(var)
+		antiguo = @head
+		@head = Node.new(var,nil,antiguo)
+	end
 	
+	def to_s
+		nodo = @head
+		string = ""
+		begin
+			string += "[#{nodo.value}]"
+			if nodo != tail
+				string += " -> "
+			end
+		end while nodo != tail
+		return string
+	end
 	
 end
