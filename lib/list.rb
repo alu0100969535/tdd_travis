83b621d7b7a4f63df3bb72d0a9230a0bb9460cb1
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-11-08 09:12:05 +0000

    Prueba 'size' list pasada

--- lib/list.rb
+++ lib/list.rb
@@ -1,16 +1,18 @@
 Node = Struct.new(:value, :next, :prev)
 
 class Lista
-	attr_reader :head,:tail
+	attr_reader :head, :tail, :size
 	
 	def initialize 
 		@head = Node.new(0,nil,nil)
 		@tail = @head
+		@size = 0
 	end
 	
 	def insert(var)
 		antiguo = @head
 		@head = Node.new(var,nil,antiguo)
+		@size += 1
 	end
 	
 end
