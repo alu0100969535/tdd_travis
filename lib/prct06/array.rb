ed1278c26525b7fd85ddceee49dfbb2b34ec89dd
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-11-28 18:42:07 +0000

    Pasada la prueba de BubbleSort con Each

--- lib/prct06/array.rb
+++ lib/prct06/array.rb
@@ -2,6 +2,7 @@ class Array
 	def bubbleSortFor
 		n = self.length
 		resultado = self
+		
 		for i in 0...n-1
 			for j in 0...n-i-1
 				if resultado[j] > resultado[j+1]
@@ -15,7 +16,22 @@ class Array
 	end
 	
 	def bubbleSortEach
-	
+		resultado = Array.new
+        resultado << self[0]
+		
+        self.drop(1).each do |i|
+            resultado.each_with_index do |j,index|
+                if i <= j
+                    resultado.insert(index,i)
+                    break
+                end
+                if index == resultado.length-1
+                    resultado << i
+                    break
+                end
+            end
+        end
+        return resultado
 	end
 
 end
