ca334668d0c9350f1f88abb9f8584c36b718c5e4
Felipe Álvarez <alu0100969535@ull.edu.es>
2017-11-29 08:38:35 +0000

    Prueba coverall2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require "coveralls"
 Coveralls.wear!
 require "bundler/setup"
 require "prct06"
-
+ 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
